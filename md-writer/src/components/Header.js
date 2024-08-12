889bd4c7b434db777b82b3834fa0dcec7d1dc9b1
aigradschool Gist <aigradschoolgist@aigradschoolui-iMac-2.local>
2024-08-12 22:48:09 +0900

    test

--- md-writer/src/components/Header.js
+++ md-writer/src/components/Header.js
@@ -30,21 +30,23 @@ const Header = ({ from }) => {
         <h1> - META WRITER - </h1>
       </div>
       <div className={styles.right}>
-      </div>
-      <div className="lnline-block">
-          <input className='toggleSideBar absolute right-0 top-0' type="checkbox" id="toggleSidebar" />
-          <img src={config.logoUrl} alt="Logo" className={styles.logoRight} />
-          <div id="sidebarMenu" className="fixed top-0 right-0 h-full w-64 bg-gradient-to-b from-pink-500 to-blue-700 transform -translate-x-0 transition-transform duration-300 ease-in-out mt-16">
-            <div class="p-6">
-                <h1 class="text-2xl font-semibold">Sidebar</h1>
-                <ul class="mt-6">
-                    <li class="mb-4"><a href="#" class="block">Home</a></li>
-                    <li class="mb-4"><a href="#" class="block">About</a></li>
-                    <li class="mb-4"><a href="#" class="block">Services</a></li>
-                    <li class="mb-4"><a href="#" class="block">Contact</a></li>
-                </ul>
-            </div>
-          </div>
+          <label>
+            <input className='toggleSideBar relative right-0 top-0' type="checkbox" id="toggleSidebar" />
+            <span >
+              <img src={config.logoUrl} alt="Logo" className="w-4 h-4 rounded-full overflow-visible hover:bg-sky-700 transition-all duration-300 ease-in-out"/>
+            </span>
+              <div id="sidebarMenu" className="fixed top-0 right-0 h-full w-30 h-[50vh] rounded-full mr-3 mt-[25vh] bg-gradient-to-b from-pink-500 to-blue-700 transform -translate-x-0 transition-transform duration-300 ease-in-out mt-16">
+                <div class="p-6">
+                     <ul class="mt-6">
+                        <li class="mb-4"><a href="#" class="block">HI</a></li>
+                        <li class="mb-4"><a href="#" class="block">re</a></li>
+                        <li class="mb-4"><a href="#" class="block">co</a></li>
+                        <li class="mb-4"><a href="#" class="block">kk</a></li>
+                    </ul>
+                </div>
+              </div>
+          </label>
+
       </div>  
     </header>
   );
